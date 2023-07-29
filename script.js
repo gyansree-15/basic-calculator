1f9a38bf5c384ae63e54ad7aaaf8b5a82f318bdc
Gyansree <102465126+gyansree-15@users.noreply.github.com>
2023-07-29 17:57:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ Array.from(buttons).forEach((button)=>{
       string=eval(string);
       document.querySelector('input').value=string;
     }
-    else if(e.target.innerHTML=='C'){
+    else if(e.target.innerHTML=='AC'){
       string=" ";
+      document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML=="DEL"){
+        string=string.substring(0,string.length-1);
       document.querySelector('input').value=string;
     }
     else{
